e485d03f4d41a5e3292037df4a754291dcccb363
William Njie <wsnjie@gmail.com>
2018-10-24 02:13:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,7 @@ var playlist = {illenium : "Gold"}
 function updatePlaylist(playlist, artistName, songTitle){
   return playlist[artistName] = songTitle
 }
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.[artistName]
+  return playlist
+}
