e73d2afbae0a8f490bb33841eba01f336a455911
William Njie <wsnjie@gmail.com>
2018-10-24 02:09:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = {illenium : "Gold"}
 function updatePlaylist(playlist, artistName, songTitle){
-  return playlist.[artistName] = songTitle
+  return playlist[artistName] = songTitle
 }
